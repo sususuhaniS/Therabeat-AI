4a7ce68307b7c560b22e3799949bf50500819cd8
sususuhaniS <suha.shar2000@gmail.com>
2026-03-09 23:24:19 -0700

    Update 04_Spotify_Playlists.py

--- pages/04_Spotify_Playlists.py
+++ pages/04_Spotify_Playlists.py
@@ -88,7 +88,7 @@ else:
                 with st.spinner("🎧 Finding your perfect playlist..."):
 
                     try:
-                        playlist_url = asyncio.run(get_spotify_playlist(predicted_genre, st.session_state.sp_client))
+                       playlist_url = get_spotify_playlist(st.session_state.sp_client, predicted_genre)
                         if playlist_url:
 
                             if "playlist_history" not in st.session_state:
