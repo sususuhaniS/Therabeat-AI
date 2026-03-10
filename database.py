7228b54229cb2ca215b5a0678b70d1a6e43e490e
sususuhaniS <suha.shar2000@gmail.com>
2026-03-09 19:13:44 -0700

    Update database.py

--- database.py
+++ database.py
@@ -148,23 +148,23 @@ def show_user_profile_form():
         exploratory = st.selectbox("Do you like exploring new music?", ['Yes', 'No'])
         foreign_languages = st.selectbox("Do you understand foreign languages?", ['No', 'Yes'])
         
-        Mood Initialization
+        # Mood Initialization
         st.markdown("### Initial Mood Settings")
         st.info("Please set your current mood. You can update this later at any time.")
         
         col1, col2 = st.columns(2)
         with col1:
-            openness = st.selectbox("Open to new experiences?", ['Yes', 'No'],
-                                   help="Are you open to trying new types of music?")
+            openness = st.selectbox("Open to new experiences?", ['Yes', 'No'], 
+                                 help="Are you open to trying new types of music?")
             anxiety = st.slider("Anxiety (1-10)", 1, 10, 5,
-                                   help="Your current anxiety level (1=low, 10=high)")
+                             help="Your current anxiety level (1=low, 10=high)")
             depression = st.slider("Mood (1-10)", 1, 10, 5,
-                                   help="Your current mood (1=low, 10=high)")
+                                help="Your current mood (1=low, 10=high)")
         with col2:
             insomnia = st.slider("Sleep Quality (1-10)", 1, 10, 5,
-                                 help="Your recent sleep quality (1=poor, 10=excellent)")
+                              help="Your recent sleep quality (1=poor, 10=excellent)")
             ocd = st.slider("Focus Level (1-10)", 1, 10, 5,
-                                help="Your current ability to focus (1=poor, 10=excellent)")
+                          help="Your current ability to focus (1=poor, 10=excellent)")
 
         music_effect = st.selectbox("Does music affect your mood?", music_effect_options)
         bpm = st.slider("Preferred BPM (Beats Per Minute)", 60, 200, 120)
@@ -257,7 +257,7 @@ def display_stored_user_data(user_profile):
                         value = user_profile[key]
                         break
                 st.metric(display_name, value)
-                
+    
     st.markdown("### Current Mood")
     with st.form("mood_form"):
         col1, col2 = st.columns(2)
@@ -321,7 +321,7 @@ def display_stored_user_data(user_profile):
                 'OCD': ocd,
                 'LastUpdated': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             }
-
+            
             # Update local profile
             user_profile.update(mood_update)
             
